16e666f4985863e85821acadb7373aca5a44edf2
Kateryna <k.churuta@gmail.com>
2024-04-08 19:51:40 +0100

    start bundle

---
+++ src/components/ui/GlobalStyle/VariableStyle.js
@@ -0,0 +1,19 @@
+export const color = {
+    white:'FFFFFF',
+    black:'000000',
+    'main-green':'B0CC0D',
+    'orange':'FFA800',
+    'dark-grey':'302929',
+    'second-dark':'0A0909',
+    grey:'6A6A6A',
+    'light-grey':'D9D9D9',
+    'green-gradient':'8EC038',
+
+}
+
+export const font = {
+    montserrat: "Montserrat, sans-serif",
+    inter: "Inter, sans-serif",
+    roboto:"Roboto, sans-serif",
+    flamenco:"Flamenco, sans-serif",
+}
